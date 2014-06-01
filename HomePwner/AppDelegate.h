ca9029bef00fc939c5befc0fc9d275e8f8a9ea42
mobileappscloud <doc92606@gmail.com>
2014-06-01 15:23:32 -0700

    Initial Commit

---
+++ HomePwner/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomePwner
+//
+//  Created by iMac on 6/1/14.
+//  Copyright (c) 2014 CongenialApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
