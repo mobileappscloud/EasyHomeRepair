1963f3c8340f64658e7bac1e36f56c85d187d23b
mobileappscloud <doc92606@gmail.com>
2014-06-02 17:43:46 -0700

    added tableview

--- HomePwner/BNRItemsViewController.m
+++ HomePwner/BNRItemsViewController.m
@@ -12,6 +12,14 @@
 
 @implementation BNRItemsViewController
 
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    [self.tableView registerClass:[UITableViewCell class]
+           forCellReuseIdentifier:@"UITableViewCell"];
+}
+
 - (instancetype) init
 {
     self = [super initWithStyle:UITableViewStylePlain];
@@ -34,6 +42,25 @@
  numberOfRowsInSection:(NSInteger)section
 {
     return [[[BNRItemStore sharedStore] allItems] count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Create an instance of UITableViewCell, with default appearance
+    UITableViewCell *cell =
+    [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"
+                                    forIndexPath:indexPath];
+    
+    // Set the text on the cell with the description of the item
+    // that is at the nth index of items, where n = row this cell
+    // will appear in on the tableview
+    NSArray *items = [[BNRItemStore sharedStore] allItems];
+    BNRItem *item = items[indexPath.row];
+    
+    cell.textLabel.text = [item description];
+    
+    return cell;
 }
 
 @end
